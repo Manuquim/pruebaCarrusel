471f777973af8895cf26b76374e2c937a5b26bb1
Manuquim <manuelcabreraosorio11@gmail.com>
2023-02-11 16:24:12 +0000

    11-02-23

--- src/js/component/PruebaCarrusel/PruebaCarrusel.jsx
+++ src/js/component/PruebaCarrusel/PruebaCarrusel.jsx
@@ -1,51 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Carousel from 'react-bootstrap/Carousel';
-//import ItemCarrusel from './ItemCarrusel.jsx';
 import "./pruebaCarrusel.css"
 
-function PruebaCarrusel() {
+function PruebaCarrusel() 
+{
+  const itemsInfo=[
+    {
+      url: "https://www.vetcomunicaciones.com.ar/uploadsfotos/animales_domesticos.jpg",
+      title:"Club Social",
+      caption: "Organizamos reuniones y pijamadas nocturnas",
+      interval: "3000",
+      height: "400"
+    },
+    {
+      url: "https://cdn.pixabay.com/photo/2019/11/07/08/40/puppy-4608266_1280.jpg",
+      title:"Paseos",
+      caption: "Programa con el cuidador la duración del mejor momento del dia para tu mascota ",
+      interval: "3000",
+      height: "400"
+    },
+    {
+      url: "https://www.dogalize.com/wp-content/uploads/2017/06/rabbit-1422882_640.jpg",
+      title:"Adiestramiento",
+      caption: "Enseña a tu macota con ayuda de un cuidador especializado",
+      interval: "3000",
+      height: "400"
+    },
+    {
+      url: "//cdn.pixabay.com/photo/2021/01/25/01/35/flying-dog-5946987_1280.jpg",
+      title:"Busca",
+      caption: "Encuentra entre nuestros cuidadores el mejor servicio especializado para tu mascota",
+      interval: "3000",
+      height: "400"
+    }
+  ];
   
   return (
     <div className="container-fluid col-6">
       <h2>GALERIA DE ACTIVIDADES</h2>
-        <Carousel>
-        <Carousel.Item>
-        <img
-          className="d-block w-100" 
-          src="https://www.vetcomunicaciones.com.ar/uploadsfotos/animales_domesticos.jpg"
-          alt="First slide"
-          height="500"
-        />
-        <Carousel.Caption>
-          <h2>First slide label</h2>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100" 
-          src="https://s1.eestatic.com/2020/02/10/como/agua-vacunas-infecciones_466466419_144847067_1706x960.jpg"
-          alt="First slide"
-          height="500"
-        />
-        <Carousel.Caption>
-          <h2>First slide label</h2>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100" 
-          src="https://www.dogalize.com/wp-content/uploads/2017/06/rabbit-1422882_640.jpg"
-          alt="First slide"
-          height="500"
-        />
-        <Carousel.Caption>
-          <h2>First slide label</h2>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>   
+        <Carousel variant="dark">
+        {
+          itemsInfo.map((item,i) =>
+          {
+            return <Carousel.Item interval={item.interval}>
+                      <img
+                        className="d-block w-100" 
+                        src={item.url}
+                        alt="slide"
+                        height={item.height}
+                      />
+                      <Carousel.Caption>
+                        <h2>{item.title}</h2>
+                        <p>{item.caption}</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+          })
+        }    
         </Carousel>
     </div>
   );
